b03e966a5922ccc03cd820698c4d66cb03358c6c
White_Night_awa <58365576+WhiteNightAWA@users.noreply.github.com>
2023-03-20 22:55:38 +0800

    Update Heroku

--- cogs/commands/music.py
+++ cogs/commands/music.py
@@ -456,6 +456,33 @@ class Music(commands.Cog):
         ctx.voice_state.loop = not ctx.voice_state.loop
         await ctx.message.add_reaction("✅")
 
+    @commands.command(name="loop")
+    async def _loop(self, ctx: commands.Context):
+        if not checkBotState(): await Close(ctx); return
+        if not ctx.voice_state.is_playing:
+            return await ctx.send("Nothing being played at the moment.")
+
+        ctx.voice_state.loop = not ctx.voice_state.loop
+        await ctx.message.add_reaction("✅")
+
+    @commands.command(name="play", aliases=["p"])
+    async def _play(self, ctx: commands.Context, *, search: str):
+        if not checkBotState(): await Close(ctx); return
+
+        if not ctx.voice_state.voice:
+            await ctx.invoke(self._join)
+
+        async with ctx.typing():
+            try:
+                source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop)
+            except YTDLError as e:
+                await ctx.send("An error occurred while processing this request: {}".format(str(e)))
+            else:
+                song = Song(source)
+
+                await ctx.voice_state.songs.put(song)
+                await ctx.send("Enqueued {}".format(str(source)))
+
     @music.command(name="play", pass_context=True, aliases=["p"])
     async def _play(self, ctx: commands.Context, *, search: str):
         if not checkBotState(): await Close(ctx); return
